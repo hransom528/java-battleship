a567bfa71539b51cf6a09eac9cd7eb780967487d
Harris Ransom <hransom528@gmail.com>
2019-11-14 15:33:44 -0500

    Update javaBattleship.java

--- java-battleship/src/javaBattleship.java
+++ java-battleship/src/javaBattleship.java
@@ -1,9 +1,18 @@
+//Java Battleship v0.1
+//By: Harris Ransom
+
+import java.util.Scanner;
 
 public class javaBattleship {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//Declares variables
+		Scanner scnr = new Scanner(System.in);
+		char[][] board = new char[11][11];
+		boolean win = false;
+		
+		//Get input to start game
+		System.out.println("Welcome to Java Battleship!");
 	}
 
 }
